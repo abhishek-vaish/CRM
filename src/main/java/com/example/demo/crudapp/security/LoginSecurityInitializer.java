14d4a54017d0cb255f3fd8de1f9e850eb792b487
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-05 14:34:45 +0530

    LoginSecurityInitializer class created

---
+++ src/main/java/com/example/demo/crudapp/security/LoginSecurityInitializer.java
@@ -0,0 +1,16 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 05-06-2020
+ *   Time: 09:46 AM
+ */
+
+
+package com.example.demo.crudapp.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class LoginSecurityInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
+
