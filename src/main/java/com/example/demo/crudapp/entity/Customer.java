0f333fc2f1ab3db78495bd721436fd50f452b342
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-06-05 14:28:12 +0530

    Entity package created with all define entity

---
+++ src/main/java/com/example/demo/crudapp/entity/Customer.java
@@ -0,0 +1,74 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 03-06-2020
+ *   Time: 10:33 AM
+ */
+
+package com.example.demo.crudapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    public Customer() {
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
